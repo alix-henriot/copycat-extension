051f1b04007dde0a824fe0bb8a6067a3fd76522d
Alix Henriot <copycat.extension@gmail.com>
2024-03-04 19:50:33 +0100

    Add of default stored values on install

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,3 +1,22 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('onInstalled....');
+import { Storage } from "@plasmohq/storage"
+ 
+const storage = new Storage()
+
+chrome.runtime.onInstalled.addListener(async () => {
+  await storage.set("clipboard", [
+    {id: 1,
+    value: "Press Alt+1"
+    },
+    {id: 2,
+    value: "Press Alt+2"
+    },
+    {id: 3,
+    value: "Press Alt+3"
+    },
+  ]);
+  await storage.set("isDockVisible", true);
+  await storage.set("isDarkMode", false);
+  await storage.set("isNotificationsEnabled", true);
+
+  console.log('Default storage values set!');
 });
