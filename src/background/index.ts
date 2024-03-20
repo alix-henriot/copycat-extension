f40b262603ecdd03c3bbc9d556e081cc2ab477b5
Alix Henriot <copycat.extension@gmail.com>
2024-03-20 13:41:16 +0100

    Adjust onInstalled functions + Edit package

--- src/background/index.ts
+++ src/background/index.ts
@@ -3,22 +3,29 @@ import { Storage } from "@plasmohq/storage"
 const storage = new Storage()
 
 chrome.runtime.onInstalled.addListener(async () => {
-  await storage.set("clipboard", [
-    {id: 1,
-    value: "Press Alt+1"
-    },
-    {id: 2,
-    value: "Press Alt+2"
-    },
-    {id: 3,
-    value: "Press Alt+3"
-    },
-  ]);
+  const os = navigator.platform;
+  console.log(os);
+  let clipboardShortcuts;
+  if (os === "MacIntel" || os === "Macintosh") {
+    clipboardShortcuts = [
+      { id: 1, value: "Option+1" },
+      { id: 2, value: "Option+2" },
+      { id: 3, value: "Option+3" },
+    ];
+  } else {
+      clipboardShortcuts = [
+        { id: 1, value: "Use Alt+1" },
+        { id: 2, value: "Use Alt+2" },
+        { id: 3, value: "Use Alt+3" },
+      ];
+  }
+  
+  await storage.set("clipboard", clipboardShortcuts);
   await storage.set("isDockVisible", true);
   await storage.set("isDarkMode", false);
   await storage.set("isNotificationsEnabled", true);
 
   console.log('Default storage values set!');
 
-  //chrome.tabs.create({ url: "https://copycat-extension.com/login" });
+  chrome.tabs.create({ url: "https://copycat-extension.com/login" });
 });
